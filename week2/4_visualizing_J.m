d5bf82574baea8aadaa6ec7c89846816c3e3c113
axayjha <akshayjha@live.in>
2018-06-10 21:02:32 +0530

    new file

--- week2/4_visualizing_J.m
+++ week2/4_visualizing_J.m
@@ -6,7 +6,6 @@ function J = computeCost(X, y, theta)
 	J = (1/(2 * m)) *sum(sq_errors);
 end
 
-
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 	m = length(y);
 	J_history = zeros(num_iters, 1);
@@ -20,14 +19,13 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 	end
 end
 
-fprintf('Running Gradient Descent ...\n');
-
 data = load('ex1data1.txt');
 y = data(:, 2);
 m = length(y);
 X = [ones(m, 1), data(:, 1)];
 
 theta = zeros(2, 1);
+
 
 iterations = 1500;
 alpha = 0.01;
@@ -36,22 +34,31 @@ computeCost(X, y, theta)
 
 theta = gradientDescent(X, y, theta, alpha, iterations);
 
-fprintf('Theta found by gradient descent: ');
-fprintf('%f %f \n', theta(1), theta(2));
+% ---------------Starts from here
 
-hold on;
-plot(X(:, 2), X*theta, '-')
-legend('Training data')
-hold off
+fprintf('Visualizing J(theta_0, theta_1 ...\n')
 
-predict1 = [1, 3.5] * theta;
-fprintf('For population = 35,000, we predict a profit of %f\n', ...
-	predict1*10000);
+theta0_vals = linspace(-10, 10, 100);
+theta1_vals = linspace(-1, 4, 100);
 
-predict2 = [1, 7] * theta;
-fprintf('For population = 70,000, we predict a profit of %f\n', ...
-	predict2*10000);
+J_vals = zeros(length(theta0_vals), length(theta1_vals));
 
-fprintf('Program paused. Press enter to continue\n');
+for i = 1:length(theta0_vals)
+	for j = 1:length(theta1_vals)
+		t = [theta0_vals(i); theta1_vals(j) ];
+		J_vals(i, j) = computeCost(X, y, t);
+	end
+end
+
+J_vals = J_vals';
+
+figure;
+surf(theta0_vals, theta1_vals, J_vals)
+xlabel('theta_0'); ylabel('theta_1');
 pause;
-
+figure;
+contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
+xlabel('theta_0'); ylabel('theta_1');
+hold on;
+plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
+pause;
